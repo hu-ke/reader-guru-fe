54a439df5ac0ee2f3cbde8c81975d1f21a393f65
HU KE <403392669@qq.com>
2024-06-08 13:51:56 +0800

    misc: update

--- src/pages/NewBook.tsx
+++ src/pages/NewBook.tsx
@@ -160,11 +160,11 @@ function NewBook() {
         {/* {selectedFile && <p>Selected file: {selectedFile.name}</p>} */}
         <Button onClick={handleUpload} loading={isUploading} disabled={!!fileInfo} size="small">
           Click to upload &nbsp;
-          <svg fill={isUploading ? theme.disabledColor : '#fff'} className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2152" width="20" height="20"><path d="M554.688 500.352v256H469.312v-256h-128L512 314.24l170.688 186.24h-128zM1024 640.192C1024 782.912 919.872 896 787.648 896h-512C123.904 896 0 761.6 0 597.504 0 451.968 94.656 331.52 226.432 302.976 284.16 195.456 391.808 128 512 128c152.32 0 282.112 108.416 323.392 261.12C941.888 413.44 1024 519.04 1024 640.192z m-259.2-205.312c-24.448-129.024-128.896-222.72-252.8-222.72-97.28 0-183.04 57.344-224.64 147.456l-9.28 20.224-20.928 2.944c-103.36 14.4-178.368 104.32-178.368 214.72 0 117.952 88.832 214.4 196.928 214.4h512c88.32 0 157.504-75.136 157.504-171.712 0-88.064-65.92-164.928-144.96-171.776l-29.504-2.56-5.888-30.976z" p-id="2153"></path></svg>
+          <svg fill={!!fileInfo ? theme.disabledColor : '#fff'} className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2152" width="20" height="20"><path d="M554.688 500.352v256H469.312v-256h-128L512 314.24l170.688 186.24h-128zM1024 640.192C1024 782.912 919.872 896 787.648 896h-512C123.904 896 0 761.6 0 597.504 0 451.968 94.656 331.52 226.432 302.976 284.16 195.456 391.808 128 512 128c152.32 0 282.112 108.416 323.392 261.12C941.888 413.44 1024 519.04 1024 640.192z m-259.2-205.312c-24.448-129.024-128.896-222.72-252.8-222.72-97.28 0-183.04 57.344-224.64 147.456l-9.28 20.224-20.928 2.944c-103.36 14.4-178.368 104.32-178.368 214.72 0 117.952 88.832 214.4 196.928 214.4h512c88.32 0 157.504-75.136 157.504-171.712 0-88.064-65.92-164.928-144.96-171.776l-29.504-2.56-5.888-30.976z" p-id="2153"></path></svg>
         </Button>
         {
           fileSizeStr ? (
-            <div>book size: {fileSizeStr}</div>
+            <div style={{fontSize: '14px'}}>book size: {fileSizeStr}</div>
           ) : ''
         }
         {
@@ -178,7 +178,7 @@ function NewBook() {
         {
           (uploadingMessage && !fileInfo) ? (
             <p style={{color: theme.green, fontStyle: 'italic'}}>
-              {uploadingMessage} <LoadingDots color='orange' />
+              {uploadingMessage} <LoadingDots color='orange' size='large' />
             </p>
           ) : ''
         }
@@ -186,7 +186,7 @@ function NewBook() {
           fileInfo ? (
             <>
               <p style={{color: theme.green, fontStyle: 'italic'}}>
-                The workspace is ready. you can summarize or chat with the chatbot now.
+                The workspace is ready. Now you can summarize or chat with the chatbot.
               </p>
               <Book>
                 <img src={fileInfo.coverImgUrl} alt="cover image" width={60}/>
