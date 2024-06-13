88c4bfc77550bcb29923f1847263d043af638f2d
HU KE <403392669@qq.com>
2024-06-13 14:33:16 +0800

    feat: language internationize

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,7 +4,11 @@ const baseUrl =  import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000'
 
 // 测试
 export const testGet = async() => {
-    let { data } = await axios.get(`${baseUrl}/api`)
+    let { data } = await axios.get(`${baseUrl}/api`, {
+        headers: {
+            lang: localStorage.getItem('language')
+        }
+    })
     return data
 }
 
@@ -12,7 +16,8 @@ export const uploadFile = async(params: any, onUploadProgress: any) => {
     let { data } = await axios.post(`${baseUrl}/api/uploadFile`, params, {
         headers: {
           'Content-Type': 'multipart/form-data',
-          deviceId: `ReaderGuru-${window.userInfo.visitorId}`
+          deviceId: `ReaderGuru-${window.userInfo.visitorId}`,
+          lang: localStorage.getItem('language')
         },
         onUploadProgress
     })
@@ -25,7 +30,8 @@ export const summarizeFile = async(filename: string) => {
     }, {
         timeout: 300000,
         headers: {
-            deviceId: `ReaderGuru-${window.userInfo.visitorId}`
+            deviceId: `ReaderGuru-${window.userInfo.visitorId}`,
+            lang: localStorage.getItem('language')
         }
     })
     return data
@@ -37,7 +43,8 @@ export const generateFileInfo = async(filename: string) => {
     }, {
         timeout: 300000,
         headers: {
-            deviceId: `ReaderGuru-${window.userInfo.visitorId}`
+            deviceId: `ReaderGuru-${window.userInfo.visitorId}`,
+            lang: localStorage.getItem('language')
         }
     })
     return data
@@ -55,7 +62,8 @@ export const queryBook = async(params: QueryBook)  => {
         query
     }, {
         headers: {
-            deviceId: `ReaderGuru-${window.userInfo.visitorId}`
+            deviceId: `ReaderGuru-${window.userInfo.visitorId}`,
+            lang: localStorage.getItem('language')
         }
     })
     return data
