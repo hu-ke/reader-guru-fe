51203d8f82f9ad2dbb8169f56c6fd9dbe97ec987
HU KE <403392669@qq.com>
2024-06-17 09:39:41 +0800

    misc: polish

--- src/i18n/zh/index.ts
+++ src/i18n/zh/index.ts
@@ -28,7 +28,7 @@ const TopBar = {
     'Mobile phone': '手机号',
     'contact me': '联系我',
     'Need help?': '需要帮助?',
-    'Your OpenAI Reading Assistant': '您的OpenAI阅读小帮手'
+    'Your OpenAI Reading Assistant': '您的OpenAI阅读助手'
 }
 
 const Menu = {
